e9d50b64acb21d1f889afd229de67117ec433050
Trent Spoolstra <tspoolst@gmail.com>
2024-01-15 22:18:11 -0700

    added lib

---
+++ matrix-library.sh
@@ -0,0 +1,523 @@
+#!/bin/bash
+#[of]:functions
+#[of]:test
+#[of]:isnum() {
+isnum() {
+#[of]:  usage
+  if false ; then
+    echo "Usage: isnum arg"
+    echo "Error: must have at least 1 argument"
+    echo "Description: checks if arg is a number"
+    echo "Examples:"
+    echo '  "if isnum 50 ; then'
+    echo '    echo is a number'
+    echo '  else'
+    echo '    echo is not a number'
+    echo '  fi"'
+    echo '  statement may also be negated'
+    echo '    "if ! isnum 50 ; then"'
+    echo "Returns:"
+    echo "  0 true"
+    echo "  1 false"
+    exit 1
+  fi
+#[cf]
+  [ "${1##*[![:digit:]+-]*}" ]
+}
+#[cf]
+#[of]:isset() {
+isset() {
+#[of]:  usage
+  if false ; then
+    echo "Usage: isset var"
+    echo "Error: must have at least 1 argument"
+    echo "Description: checks if a given variable is set (i.e. exist)"
+    echo "Examples:"
+    echo '  "if isset your_var ; then'
+    echo '    echo your variable is set'
+    echo '  else'
+    echo '    echo your variable is not set'
+    echo '  fi"'
+    echo '  statement may also be negated'
+    echo '    "if ! isset your_var ; then"'
+    echo "Returns:"
+    echo "  0 true"
+    echo "  1 false"
+    exit 1
+  fi
+#[cf]
+  eval "[ \"\${$1+1}\" ]"
+}
+#[cf]
+#[cf]
+#[of]:array
+#[of]:asize() {
+  asize() {
+    if [[ "$1" = "-" ]] ; then
+      eval "echo \${#${2}[@]}"
+    else
+      eval "$1=\"\${#${2}[@]}\""
+    fi
+  }
+#[cf]
+#[of]:akeys() {
+  akeys() {
+    if [[ "$1" = "-" ]] ; then
+      eval "echo \"\${!${2}[@]}\""
+    else
+      eval "$1=\"\${!${2}[@]}\""
+    fi
+  }
+#[cf]
+#[of]:aget() {
+  aget() {
+    local _i
+    if [ -z "${2##*\[*}" ] ; then
+      if [ "$1" = "-" ] ; then
+        eval "echo \"\${$2}\""
+      else
+        eval "$1=\"\${$2}\""
+      fi
+    else
+      _i=$(eval "for _i in \"\${${2}[@]}\" ; do
+        echo -n \"'\${_i//\\'/\\'\\\\\'\\'}' \"
+      done")
+      if [ "$1" = "-" ] ; then
+        echo "${_i% }"
+      else
+        eval "$1=\"\${_i% }\""
+      fi
+    fi
+  }
+#[cf]
+#[of]:astep() {
+  astep() {
+#[c]    astep v a 1
+#[c]    astep v a
+#[c]    astep a 1
+#[c]    astep a
+
+    #force local vars to be defined as unset without affecting parent vars in ksh/bash/dash
+    local _v _a _i _s
+    unset _v _a _i _s
+    local _v _a _i _s
+    case $# in
+      3)
+        _v="$1"
+        _a="$2"
+        _s="$3"
+        ;;
+      2)
+        if isnum "$2" ; then
+          _a="$1"
+          _s="$2"
+        else
+          _v="$1"
+          _a="$2"
+        fi
+        ;;
+      1)
+        _a=$1
+        ;;
+    esac
+    isset "${_a}" || return 1
+    ! isset "${_a}_size" && asize ${_a}_size "${_a}"
+    if isset "${_a}_index" ; then
+      eval ": \$((${_a}_index=\${${_a}_index:-0}+${_s:-1}))"
+      if eval "[ \${${_a}_index} -lt 0 -o \${${_a}_index} -ge \${${_a}_size} ]" ; then
+        areset "${_a}"
+        return 1
+      fi
+    else
+      eval ${_a}_index=0
+    fi
+    isset "_v" && eval aget \"\${_v}\" \"${_a}[\${${_a}_index:-0}]\"
+  }
+#[cf]
+#[of]:areset() {
+  areset() {
+    unset $1_index $1_size
+    [ $# -eq 2 ] && eval $1_index=\"\$2\"
+  }
+#[cf]
+#[of]:acurrent() {
+  acurrent() {
+    if isset "$2" && isset "$2_index" ; then
+      eval aget \"\$1\" \"\${2}[\${${2}_index}]\"
+    else
+      return 1
+    fi
+  }
+#[cf]
+#[of]:isaheader() {
+  isaheader() {
+    isset "$1" && ! isarray "$1"
+  }
+#[cf]
+
+#[c]
+#[of]:array ksh
+if [ -n "${KSH_VERSION}" ] ; then
+#[of]:  aset() {
+  aset() {
+    local _n
+    _n="${1##=*}"
+    if [ -z "${_n##*\[*}" ] ; then
+      eval "${1}=\"\${2}\""
+    else
+      eval "
+        shift
+        set -A $1 -- \"\$@\"
+      "
+    fi
+  }
+#[cf]
+#[of]:  aunset() {
+    aunset() {
+      eval $(eval "set | while read -r _i;do [ -z \"\${_i##$1_*}\" ] && echo \"unset \\\"\${_i%%=*}\\\"\";done")
+      unset $1
+    }
+#[cf]
+#[of]:  isarray() {
+  isarray() {
+    local _i
+    _i=$(typeset -p "$1" 2>/dev/null)
+    [ -n "${_i}" -a -z "${_i##set -A *}" ]
+  }
+#[cf]
+#[cf]
+#[of]:array bash
+elif [ -n "${BASH_VERSION}" ] ; then
+#[of]:  aset() {
+  aset() {
+    local _n
+    _n="${1##=*}"
+    if [ -z "${_n##*\[*}" ] ; then
+      eval "${1}=\"\${2}\""
+    else
+      eval "
+        shift
+        $1=(\"\$@\")
+      "
+    fi
+  }
+#[cf]
+#[of]:  aunset() {
+  aunset() {
+    eval unset \"\$1\" \${!${1}_*}
+  }
+#[cf]
+#[of]:  isarray() {
+  isarray() {
+    local _i
+    _i=$(typeset -p "$1" 2>/dev/null)
+    [ -n "${_i}" -a -z "${_i##declare -a *}" ]
+  }
+#[cf]
+#[cf]
+else
+#[of]:  array dash
+  if ! type '[[' >/dev/null ; then
+#[of]:    aset() {
+  aset() {
+    local _n _i
+    _n="$1"
+    if [ -z "${_n##*\[*}" ] ; then
+      _n="${1%%\[*}"
+      [ -n "${_n##*_*}" ] && ! isset "${_n}" && eval ${_n}=1
+      _i="${1##${_n}\[}";_i="${_i%%\]*}"
+      eval "${_n}_${_i}=\"\${2}\""
+    else
+      aunset "$1"
+      _n="$1";shift
+      [ -n "${_n##*_*}" ] && ! isset "${_n}" && eval ${_n}=1
+      _i=0
+      while [ $# -gt 0 ] ; do
+        eval "${_n}_${_i}=\"\$1\""
+        shift
+        : $((_i=_i+1))
+      done
+    fi
+  }
+#[cf]
+#[of]:    asize() {
+  asize() {
+    local _i
+    eval "_i=\$(set | while read -r _i;do [ -z \"\${_i##${2}_[[:digit:]]*}\" ] && echo -n 1;done)"
+    _i="${#_i}"
+    if [ "$1" = "-" ] ; then
+      echo "${_i}"
+    else
+      eval "$1=\"\${_i}\""
+    fi
+  }
+#[cf]
+#[of]:    aunset() {
+  aunset() {
+    eval $(eval "set | while read -r _i;do [ -z \"\${_i##$1_*}\" ] && echo \"unset \\\"\${_i%%=*}\\\"\";done")
+    unset $1
+  }
+#[cf]
+#[of]:    akeys() {
+  akeys() {
+    local _i
+    eval "_i=\$(set | while read -r _i;do [ -z \"\${_i##${2}_*}\" ] && _i=\"\${_i%%=*}\" && echo -n  \"\${_i##*_} \";done)"
+    if [ "$1" = "-" ] ; then
+      eval "echo \"${_i% }\""
+    else
+      eval "$1=\"\${_i% }\""
+    fi
+  }
+#[cf]
+#[of]:    aget() {
+  aget() {
+    local _i _n
+    if [ -z "${2##*\[*}" ] ; then
+      _n="${2%%\[*}"
+      _i="${2##${_n}\[}";_i="${_i%%\]*}"
+      if [ "$1" = "-" ] ; then
+        eval "echo \"\${${_n}_${_i}}\""
+      else
+        eval "$1=\"\${${_n}_${_i}}\""
+      fi
+    else
+      eval "_i=\$(set | while read -r _i;do [ -z \"\${_i##${2}_*}\" ] && echo -n \"\${_i#*=} \";done)"
+      if [ "$1" = "-" ] ; then
+        eval "echo \"\${_i% }\""
+      else
+        eval "$1=\"\${_i% }\""
+      fi
+    fi
+  }
+#[cf]
+#[of]:    isarray() {
+  isarray() {
+    local _i
+    eval "_i=\$(set | while read -r _i;do [ -z \"\${_i##${1}_*}\" ] && echo 1 && break;done)"
+    [ "${_i}" = 1 ]
+  }
+#[cf]
+#[of]:    isaheader() {
+  isaheader() {
+    isarray "$1"
+  }
+#[cf]
+#[cf]
+  else
+    echo "not sure what shell this is"
+  fi
+fi
+#[cf]
+#[of]:matrix
+#[of]:mset() {
+mset() {
+  local _an _dm _in
+  _an="$1";shift
+  _dm="$1";shift
+  [ ${_dm} -gt 1 ] && {
+    eval ${_an}="${_dm}"
+  }
+  _in=${_dm}
+  while [ ${_in} -gt 1 ] ; do
+    _an="${_an}_$1"
+    shift
+    : $((_in=_in-1))
+  done
+
+  if [ $# -eq 2 ] ; then
+    _in="$1"
+    shift
+    aset ${_an}[${_in}] "$1"
+    return $?
+  elif [ $# -gt 2 ] ; then
+    shift
+    aset ${_an} "$@"
+    return $?
+  fi
+  echo "not enough arguments for matrix size" >&2
+  return 1
+}
+#[cf]
+#[of]:mget() {
+mget() {
+  local _v _an _dm _in
+  _v="$1";shift
+  _an="$1";shift
+  
+  if ! isset "${_an}" ; then
+    _dm=1
+  else
+    eval "_dm=\"\${${_an}}\""
+  fi
+
+  _in="${_dm}"
+
+  while [ ${_in} -gt 1 ] ; do
+    [ $# -gt 0 ] && { _an="${_an}_$1";shift; } || break
+    : $((_in=_in-1))
+  done
+
+  if [ ${_in} -gt 1 ] ; then
+    echo "not enough arguments for matrix size" >&2
+    return 1
+  elif [ $# = 0 ] ; then
+    aget "${_v}" "${_an}"
+    return $?
+  elif [ $# = 1 ] ; then
+    _in="$1";shift
+    aget "${_v}" "${_an}[${_in}]"
+    return $?
+  fi
+}
+#[cf]
+#[of]:msize() {
+msize() {
+  local _v _an _dm _in
+  _v="$1";shift
+  _an="$1";shift
+
+  if ! isset "${_an}" ; then
+    _dm=1
+  else
+    eval "_dm=\"\${${_an}}\""
+  fi
+
+  _in="${_dm}"
+  [ $# -gt $((_in-1)) ] && { echo "matrix is not that big" >&2;return 1; }
+  while [ ${_in} -gt 1 ] ; do
+    [ $# -gt 0 ] && { _an="${_an}_$1";shift; } || break
+    : $((_in=_in-1))
+  done
+  
+  [ ${_in} -eq 1 ] && asize ${_v} "${_an}"
+  [ ${_in} -gt 1 ] && {
+    _in=$( set | (
+        _j=0;_k=""
+        while read -r _i;do
+          [ -z "${_i##${_an}_*}" ] && {
+            _i="${_i%%[\[=]*}"
+            _i="${_i#${_an}_}"
+            _i="${_i%%_*}"
+            [ ${_j} -eq 0 ] && { _k="${_i}";: $((_j=_j+1));continue; }
+            [ "${_k}" != "${_i}" ] && { _k="${_i}";: $((_j=_j+1)); }
+          }
+        done
+        echo ${_j}
+      )
+    )
+    if [ "${_v}" = "-" ] ; then
+      eval "echo \"${_in% }\""
+    else
+      eval "${_v}=\"\${_in% }\""
+    fi
+  }
+
+  return
+}
+#[cf]
+#[of]:mstep() {
+mstep() {
+  #force local vars to be defined as unset without affecting parent vars in ksh/bash/dash
+  local _dm _v _a _i _s
+  unset _dm _v _a _i _s
+  local _dm _v _a _i _s
+  
+  ! isnum "$2" && { _v="$1";shift; }
+  _a="$1"
+  isset "${_a}" || return 1
+  isaheader "${_a}" && eval _dm=\"\${${_a}:-1}\" || _dm=1
+
+  shift
+  while [ ${_dm} -gt 1 ] ; do
+    [ $# -eq 0 ] && { echo "not enough args for this matrix/array" >&2;return 1; }
+    _a="${_a}_$1"
+    : $((_dm=_dm-1))
+    shift
+  done
+  _s="$1"
+
+  ! isset "${_a}_size" && asize ${_a}_size "${_a}"
+  if isset "${_a}_index" ; then
+    eval ": \$((${_a}_index=\${${_a}_index:-0}+${_s:-1}))"
+    if eval "[ \${${_a}_index} -lt 0 -o \${${_a}_index} -ge \${${_a}_size} ]" ; then
+      areset "${_a}"
+      return 1
+    fi
+  else
+    eval ${_a}_index=0
+  fi
+  isset "_v" && eval aget \"\${_v}\" \"${_a}[\${${_a}_index:-0}]\"
+}
+#[cf]
+#[of]:mreset() {
+mreset() {
+  #force local vars to be defined as unset without affecting parent vars in ksh/bash/dash
+  local _dm _v _a _i _s
+  unset _dm _v _a _i _s
+  local _dm _v _a _i _s
+  
+  _a="$1"
+  isset "${_a}" || return 1
+  isaheader "${_a}" && eval _dm=\"\${${_a}:-1}\" || _dm=1
+  
+  shift
+  while [ ${_dm} -gt 1 ] ; do
+    [ $# -eq 0 ] && { echo "not enough args for this matrix/array" >&2;return 1; }
+    _a="${_a}_$1"
+    : $((_dm=_dm-1))
+    shift
+  done
+
+  unset ${_a}_index ${_a}_size
+  [ $# -eq 1 ] && eval ${_a}_index=\"\$1\"
+}
+#[cf]
+#[of]:mcurrent() {
+mcurrent() {
+  #force local vars to be defined as unset without affecting parent vars in ksh/bash/dash
+  local _dm _v _a
+  unset _dm _v _a
+  local _dm _v _a
+  
+  _v="$1"
+  shift
+  _a="$1"
+  isset "${_a}" || return 1
+  isaheader "${_a}" && eval _dm=\"\${${_a}:-1}\" || _dm=1
+  
+  shift
+  while [ ${_dm} -gt 1 ] ; do
+    [ $# -eq 0 ] && { echo "not enough args for this matrix/array" >&2;return 1; }
+    _a="${_a}_$1"
+    : $((_dm=_dm-1))
+    shift
+  done
+
+  if isset "${_a}_index" ; then
+    eval aget \"\${_v}\" \"\${_a}[\${${_a}_index}]\"
+  else
+    return 1
+  fi
+}
+#[cf]
+#[c]
+#[of]:matrix ksh
+if [ -n "${KSH_VERSION}" ] ; then
+  :
+#[cf]
+#[of]:matrix bash
+elif [ -n "${BASH_VERSION}" ] ; then
+  :
+#[cf]
+else
+#[of]:  matrix dash
+  if ! type '[[' >/dev/null ; then
+    :
+#[cf]
+  else
+    echo "not sure what shell this is"
+  fi
+fi
+#[cf]
+#[cf]
+
---
+++ matrix-test.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+#[of]:case "$1" in
+case "$1" in
+  -k)
+    ksh "$0" -s
+    exit
+    ;;
+  -b)
+    bash "$0" -s
+    exit
+    ;;
+  -d)
+    dash "$0" -s
+    exit
+    ;;
+  -s)
+    ;;
+  *)
+    exit
+    ;;
+esac
+#[cf]
+. ./matrix-library.sh
+
+#[of]:arrayTest () {
+arrayTest () {
+  aset a_1 9
+  
+  aset a_0 1 2 3 4 5
+  aset a_0[6] "6 6"
+  
+  set | grep -P '^a(=|_)'
+  echo
+#[c]  aunset a_0
+#[c]  echo
+  asize b a_0
+  echo $b
+  echo
+  akeys - a_0
+  for i in $(akeys - a_0);do echo "${i}";done
+  aset a_0[2] "3 4"
+#[c]  set | grep -P '^a(=|_)'
+  aget - a_0[2]
+  aget - a_0
+#[c]  exit
+  eval "for i in $(aget - a_0);do echo \"\${i}\";done"
+  echo
+  aget - a_1
+  
+  aget m a_1
+  echo "${m}"
+
+  echo
+  aset a 1
+  isarray a && echo yes || echo no
+  isarray b && echo yes || echo no
+
+#[c]  aset aa 2 5
+#[c]  isarray aa && echo yes
+#[c]  set | grep ^aa
+#[c]  exit
+  
+  aunset aa
+  aset aa 6 5 '4 "4' 3 "2 '2" 1
+  
+  areset aa $(($(asize - aa)))
+  while astep b aa -1;do echo "${b}";done
+  echo
+#[c]  while astep b aa;do acurrent b aa;echo "${b}";done
+  while astep b aa;do acurrent - aa;done
+#[c]  
+  echo
+  areset aa 1
+  acurrent - aa && echo yes || echo no
+  echo
+  set | grep ^aa
+
+  exit
+}
+#[cf]
+#[of]:matrixTest() {
+matrixTest() {
+#[c]  set -xv
+  mset aa 1 _ 1 2 3 4
+#[c]  exit
+  mset bb 2 0 _ 1 2 3 4
+  mset bb 2 1 _ 5 6 7 8
+  mset cc 4 0 0 0 _ a b c d
+  mset cc 4 0 1 2 _ e f g h
+  mset cc 4 0 2 1 _ i j k l
+  mset cc 4 0 3 3 _ m n o p
+  
+  set | grep -P '^(aa|bb|cc|tt)(\[|=|_)'
+#[c]  exit
+  
+#[c]  set -xv
+#[c]  mget - aa 3
+#[c]  mget tt cc 0 2 1
+  mget tt cc 0 2 1 2
+  echo "--${tt}--"
+#[c]  exit
+  
+#[c]  set -xv
+#[c]  aunset aa
+#[c]  mset aa 1 0 a
+#[c]  mset aa 1 _ a b c d e
+#[c]  mset aa 2 0 _ a b c d e f
+#[c]  mset aa 2 1 _ a b c d e f g h
+  mset aa 4 0 0 0 _ a b c d e
+  mset aa 4 0 0 1 _ a b c d
+  mset aa 4 1 0 0 _ a b c
+  mset aa 4 2 0 0 _ a b c d e f
+  
+#[c]    set | grep -P '^(aa|bb|cc|tt)(\[|=|_)'
+  
+  msize - aa 0 0 0
+  msize - aa 0 0 1
+  msize - aa 0 0
+  msize - aa 0
+  msize - aa
+
+
+#[c]5
+#[c]4
+#[c]2
+#[c]1
+#[c]3
+
+
+  mset aa 4 3 2 1 _ 1 2 3 '4 "4' "5' 5" 
+  set | grep ^aa
+  mget bb aa 3 2 1
+  set | grep ^bb
+  echo "${bb}"
+  eval set -- "${bb}"
+  echo "$# -- $@"
+#[c]  set -xv
+  mstep - aa 3 2 1
+  mstep - aa 3 2 1
+  mstep - aa 3 2 1
+  mstep - aa 3 2 1
+#[c]  set -xv
+  mcurrent - aa 3 2 1
+  
+  mreset aa 3 2 1 2
+  mcurrent - aa 3 2 1
+  
+  echo
+  mreset aa 3 2 1
+  while mstep - aa 3 2 1;do :;done
+  echo
+  mreset aa 3 2 1 $(msize - aa 3 2 1)
+  while mstep - aa 3 2 1 -1;do :;done
+  
+  
+  exit
+  
+#[c]  aunset aa
+#[c]  aset aa 6 5 4 3 2 1
+  set | grep ^aa
+#[c]  
+#[c]  mstep - aa
+#[c]  mstep - aa
+#[c]  mstep - aa
+  
+#[c]  while mstep - aa ; do :; done
+
+
+
+  exit
+}
+#[cf]
+
+#[c]arrayTest
+#[c]matrixTest
+
+aset a[2] 5
+aset a[5] 3
+asize - a
+akeys - a
+aget - a
+
+mget - a
+
+
+#[c]
